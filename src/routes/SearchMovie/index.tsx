083fd4c3ac9a65785dc8c3d8c7b6eb476e8f88b4
seong-doo <85680496+seong-doo@users.noreply.github.com>
2022-05-22 03:29:18 +0900

    last last last..

--- src/routes/SearchMovie/index.tsx
+++ src/routes/SearchMovie/index.tsx
@@ -20,7 +20,7 @@ const SearchMovie = () => {
 
   axios
     .get(
-      `${PROXY}?ServiceKey=JDxWm/ZI8QYLW3HekZ2yqKFqcHedPCx1tS/Dyf9wDzQhzm9KS6t3Sb61Hx2/MAMuLmkOtktLdV5Mn4k4sKHVNA==&searchText=암`
+      `/B551182/diseaseInfoService/getDissNameCodeList${PROXY}?ServiceKey=JDxWm/ZI8QYLW3HekZ2yqKFqcHedPCx1tS/Dyf9wDzQhzm9KS6t3Sb61Hx2/MAMuLmkOtktLdV5Mn4k4sKHVNA==&searchText=암`
     )
     .then((res) => {
       // eslint-disable-next-line
