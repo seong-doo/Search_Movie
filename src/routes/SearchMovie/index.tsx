21b632477a5a866fc2fce4c9401d2e49495e1cd0
seong-doo <85680496+seong-doo@users.noreply.github.com>
2022-05-22 02:53:13 +0900

    update

--- src/routes/SearchMovie/index.tsx
+++ src/routes/SearchMovie/index.tsx
@@ -19,7 +19,7 @@ const SearchMovie = () => {
   const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 
   axios({
-    url: `${PROXY}/B551182/diseaseInfoService/getDissNameCodeList`, // 통신할 웹문서
+    url: `${PROXY}`, // 통신할 웹문서
     method: 'get', // 통신할 방식
     data: {
       // 인자로 보낼 데이터
